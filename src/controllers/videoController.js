ff9a404c37cb6a9e496e349f5c17e1527267414c
happysubin <76802855+happysubin@users.noreply.github.com>
2021-05-11 00:56:49 +0900

    template 3 (Iteration)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,8 +11,38 @@ export const editVideo = (req, res) => {
   return res.render("editVideo");
 };
 export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home", fakeUser });
+  const videos = [
+    {
+      title: "Hello",
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Whatsup",
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
+
 export const search = (req, res) => {
   res.send("search");
 };
@@ -20,6 +50,7 @@ export const search = (req, res) => {
 export const upload = (req, res) => {
   res.send("upload");
 };
+
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   return res.send("delete video");
