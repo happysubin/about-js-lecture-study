8ee602533cee08b93e2a9db6e99f9b61a2d4e153
happysubin <76802855+happysubin@users.noreply.github.com>
2021-05-11 18:20:54 +0900

    fake db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,46 +1,44 @@
-const fakeUser = {
-  name: "samsung",
-  loggedIn: true,
-};
+let videos = [
+  {
+    title: "Hello",
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Whatsup",
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 
 export const see = (req, res) => {
-  return res.render("see");
+  const { id } = req.params;
+  const video = videos[id - 1]; // id를 이용해서 video를 찾는다. 배열 인덱스는 0 부터 시작 주의
+  return res.render("see", { pageTitle: `watch ${video.title}`, video });
 };
 export const editVideo = (req, res) => {
   console.log(req.params);
   return res.render("editVideo");
 };
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Hello",
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Whatsup",
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
-  res.render("home", { pageTitle: "Home", fakeUser, videos });
+  res.render("home", { pageTitle: "Home", videos });
 };
 
 export const search = (req, res) => {
