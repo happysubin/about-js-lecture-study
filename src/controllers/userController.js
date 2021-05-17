0cad6bd1c7a2e91c3aed8a64681905724b5c1e6d
happysubin <76802855+happysubin@users.noreply.github.com>
2021-05-17 23:03:28 +0900

    create account
    hash password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,27 @@
+import User from "../models/User";
 export const edit = (req, res) => {
   res.send("Edit User");
 };
 export const deleteUser = (req, res) => {
   res.send("Delete");
 };
-export const join = (req, res) => {
-  res.send("join");
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+  }
 };
 export const login = (req, res) => {
   res.send("login");
