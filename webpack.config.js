45bc5373f3309a4648367017fb18884b04c20433
happysubin <76802855+happysubin@users.noreply.github.com>
2021-07-25 00:50:47 +0900

    webpack part 2 mini-css-extract-plugin
    main css 자동 생성됨. plugin으로 인해서, pug에서 css 연결

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
@@ -5,11 +6,16 @@ module.exports = {
   mode: "development", //모드가 개발중이다! 즉 아직 개발중이다 를 의미 (default 값은 production mode이다  완성되었으면 production mode를 사용)
   output: {
     //webpack 은 절대경로를 요구한다!!!!!!
-    filename: "main.js", //path 안에 저장된 파일. 결과물을 위한 파일이름을 쓰고
-    path: path.resolve(__dirname, "assets", "js"), //파일을 어디에 저장할지 작성하는 부분! assets dir 안에 js dir 을 의미
+    filename: "js/main.js", //path 안에 저장된 파일. 결과물을 위한 파일이름을 쓰고
+    path: path.resolve(__dirname, "assets"), //파일을 어디에 저장할지 작성하는 부분! assets dir 안에 js dir 을 의미
     //1. path 를 작성할 당시 assets 라는 폴더가 없다
     //2. npm run assets 를 통해 assets js 폴더도 생기고 main.js 라는 파일도 자동으로 생겼다.
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/main.css",
+    }),
+  ], //플러그인을 사용
   module: {
     rules: [
       {
@@ -24,8 +30,10 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"], //실행되는 역순으로 코드를 작성한다. 이유는 webpack은 뒤에서부터 시작하기 때문
-        //실상은 sass-loader 부터 실행된다
+        //style-loader을 대체!!!! main.css 파일이 생성된다
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], //이건 js에서 css를 적용하는게 아니라 css를 모은 파일을 생성한다
+        //use: ["style-loader", "css-loader", "sass-loader"], //실행되는 역순으로 코드를 작성한다. 이유는 webpack은 뒤에서부터 시작하기 때문
+        //실상은 sass-loader 부터 실행된다  , 이 코드를 통해 js 파일안에서 scss가 css로 바뀌고 css가 적용된다
       },
     ],
   },
