8cc1ad6e2d78b28a15eb846b7138f156ef2bc3ff
happysubin <76802855+happysubin@users.noreply.github.com>
2021-07-26 18:26:00 +0900

    styling part

--- Loading.js
+++ Loading.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native"; //이것들을 꼭 import 해야해!!!
+import { StyleSheet, Text, View, StatusBar } from "react-native"; //이것들을 꼭 import 해야해!!!
 
 export default function Loading() {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
       <Text style={styles.text}>Getting the nice Weather!!</Text>
     </View>
   );
