f93567e2517a0bf2c2fc1af13ce365b1c7b1c026
happysubin <76802855+happysubin@users.noreply.github.com>
2021-07-26 16:07:31 +0900

    get latitude, longitude and fix error
    accuracy 옵션을 주니 getCurrent 메소드가 작동! 오류 해결

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import { Alert } from "react-native";
+
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
 export default class extends React.Component {
-  getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+  state = {
+    isLoading: true,
   };
+
+  getLocation = async () => {
+    try {
+      const per = await Location.requestBackgroundPermissionsAsync(); //이 메소드를 통해 유저의 동의를 구한다.
+      //여기서 에러나면 바로 catch 문으로
+      console.log("permission", per);
+      // 여기 이후부터 작동이 안된다 대박 사건?!?!?!!? option accuracy를 주니까 해결됨 이게 무슨일이람!!!
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 4 });
+      console.log(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Can't find you..", "So sad");
+    }
+  };
+
   componentDidMount() {
     this.getLocation();
   }
+  componentDidUpdate() {}
+  componentWillUnmount() {}
+
   render() {
-    return <Loading></Loading>;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
+
+//ctrl + m 은 개발자 모드로 직행한다!
