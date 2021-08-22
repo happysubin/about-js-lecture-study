0beb6d28b614f87c6e748c24e2efb145c9c6f1d8
happysubin <76802855+happysubin@users.noreply.github.com>
2021-08-22 17:08:47 +0900

    Comment Ids

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,16 +1,20 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addComment = (text) => {
+const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
+  newComment.dataset.id = id;
   newComment.className = "video__comment";
   const icon = document.createElement("i");
   icon.className = "fas fa-comment";
   const span = document.createElement("span");
   span.innerText = ` ${text}`;
+  const span2 = document.createElement("span");
+  span2.innerText = "❌";
   newComment.appendChild(icon);
   newComment.appendChild(span);
+  newComment.appendChild(span2);
   videoComments.prepend(newComment);
 };
 
@@ -24,15 +28,18 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     //헤더는 req요청을 할 때 부가적인 정보, 세부사항 을 가지고 있다.
     //"Content-Type": "application/json"  우리는 이를 통해 백엔드에 json 형식을 전달한다는 의미를 포함하고 있다!
     body: JSON.stringify({ text }), //fetch 로는 보통 json 데이터를 보낸다.json 형식을 문자열로 바꾸어서 백엔드에 요청을 보낸다
   }); //from 과 같이 req.body로 보낸다. from 없이 req.body로 보내는 방법!
-  textarea.value = "";
-  if (status === 201) addComment(text);
+  if (response.status === 201) {
+    textarea.value = "";
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
+  }
 };
 
 //두번째 해결방법 form이 있는 지 검사
