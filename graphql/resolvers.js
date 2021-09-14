31b5b4a297a2c317362604c1bb1be23a353eb564
happysubin <76802855+happysubin@users.noreply.github.com>
2021-09-15 00:50:44 +0900

    upgrade schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
+import { people } from "./db";
 const resolvers = {
   Query: {
-    name: () => "Devbin",
+    people: () => people,
   },
 };
 
 //쿼리랑 resolvers 가 필요
+//playground 는 postman 과 비슷.
 export default resolvers;
