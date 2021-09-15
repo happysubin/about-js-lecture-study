d60e4c9ab78627d44529e651dbb8e774658cec74
happysubin <76802855+happysubin@users.noreply.github.com>
2021-09-15 17:15:17 +0900

    creating Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,16 @@
-import { getById, people } from "./db";
+import { getById, getMovies, deleteMovie, addMovie } from "./db";
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, { id }) => getById(id),
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
   },
 };
 
 //쿼리랑 resolvers 가 필요
 //playground 는 postman 과 비슷.
+//argument 를 전달
+//mutation 은 cud를 실행 query는 주로 r read를 실행
 export default resolvers;
