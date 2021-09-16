7c2be320bb0a42cd18834914ec16a3d1b21dac83
happysubin <76802855+happysubin@users.noreply.github.com>
2021-09-17 00:15:03 +0900

    update

--- graphql/db.js
+++ graphql/db.js
@@ -1,19 +1,45 @@
 import axios from "axios";
-const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+const BASE_URL = "https://yts-proxy.now.sh/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
 export const getMovies = async (limit, rating) => {
-  let REQUEST_URL = API_URL;
-  if (limit > 0) {
-    REQUEST_URL += `limit=${limit}`;
-  }
-  if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`;
-  }
-
   const {
     data: {
       data: { movies },
     },
-  } = await axios.get(`${REQUEST_URL}`);
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
+};
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
+};
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
   return movies;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,16 @@
-import { getMovies } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
+
 const resolvers = {
   Query: {
-    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
+
+export default resolvers;
 
 //쿼리랑 resolvers 가 필요
 //playground 는 postman 과 비슷.
 //argument 를 전달
 //mutation 은 cud를 실행 query는 주로 r read를 실행
-export default resolvers;
