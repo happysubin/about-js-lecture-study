f2455fee49677fb998008d38e7eaa79fade263ed
happysubin <76802855+happysubin@users.noreply.github.com>
2021-09-18 19:26:23 +0900

    recap graphql

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "babe",
+    movies: () => [],
+    movie: () => ({ title: "hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (root, { title }, info, context) => {
+      console.log(root);
+      console.log(info);
+      console.log(context);
+      console.log(title);
+      return true;
+    },
+    //리졸버와 스키마의 프로퍼티 순서도 아예 같아야한다!
+    //createMovie:(root,args,info,context)=>{},
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
