4846545de69bb4d31e4a17ce9751ad9e462a04c6
happysubin <76802855+happysubin@users.noreply.github.com>
2021-09-20 02:00:30 +0900

    seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -9,32 +9,38 @@ export default {
       { firstName, lastName, username, email, password }
     ) => {
       // check if username or email are already on DB.
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      console.log(existingUser); //겹치는 사람이 없으면 null이 나온다
-      // hash password
-      const hashPassword = await bcrypt.hash(password, 10); // 해싱
-      // save and return the user
-      return client.user.create({
-        data: {
-          //이렇게 data로 저장하는걸 주의!!!
-          username,
-          email,
-          firstName,
-          lastName,
-          password: hashPassword,
-        },
-      });
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser)
+          throw new Error("This username/email is already taken");
+        console.log(existingUser); //겹치는 사람이 없으면 null이 나온다
+        // hash password
+        const hashPassword = await bcrypt.hash(password, 10); // 해싱
+        // save and return the user
+        return client.user.create({
+          data: {
+            //이렇게 data로 저장하는걸 주의!!!
+            username,
+            email,
+            firstName,
+            lastName,
+            password: hashPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
