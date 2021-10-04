e4758905b626efed944a9812bb62dc7e148670bd
happysubin <76802855+happysubin@users.noreply.github.com>
2021-10-04 17:43:25 +0900

    ts class

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-interface human {
-  name: string;
-  age: number;
-  married: boolean;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public married: boolean;
+  public gender: string;
+  constructor(name: string, age: number, married: boolean, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.married = married;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "k",
-  age: 24,
-  married: false,
-  gender: "male",
-};
+const bin = new Human("bin", 24, false, "male");
 
-const helloWorld = (person: human): string =>
+const helloWorld = (person: Human): string =>
   `${person.name}, ${person.age}, ${person.married}, ${person.gender} !!`; //반환 값을 알려준다
 
-console.log(helloWorld(person));
+console.log(helloWorld(bin));
