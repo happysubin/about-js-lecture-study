a581defc5c548f867e9191cc7048d2392bb94d17
happysubin <76802855+happysubin@users.noreply.github.com>
2021-10-04 17:33:28 +0900

    interface

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-const helloWorld = (
-  name: string,
-  age: number,
-  married: boolean,
-  gender: string
-): string => `${name}, ${age}, ${married}, ${gender}`; //반환 값을 알려준다
+interface human {
+  name: string;
+  age: number;
+  married: boolean;
+  gender: string;
+}
 
-console.log(helloWorld("k", 24, false, "ho"));
+const person = {
+  name: "k",
+  age: 24,
+  married: false,
+  gender: "male",
+};
+
+const helloWorld = (person: human): string =>
+  `${person.name}, ${person.age}, ${person.married}, ${person.gender} !!`; //반환 값을 알려준다
+
+console.log(helloWorld(person));
