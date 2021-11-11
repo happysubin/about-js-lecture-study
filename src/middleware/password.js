8933941a528ec06ed4587c2a98c6753337b515bf
happysubin <asb0711@gmail.com>
2021-11-11 23:16:51 +0900

    make password part

---
+++ src/middleware/password.js
@@ -0,0 +1,46 @@
+import crypto from "crypto";
+
+//node js 교과서 125 126
+
+export const createSalt = async () => {
+  try {
+    return await crypto.randomBytes(64).toString("base64");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//랜덤으로 64바이트 길이의 문자열을 만든다.
+
+export const createHashPassword = (originPassword) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const salt = await createSalt();
+      crypto.pbkdf2(originPassword, salt, 10000, 64, "sha512", (err, key) => {
+        if (err) throw err;
+        resolve({ hashedPassword: key.toString("base64"), salt });
+      });
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  });
+
+//salt 와 해쉬화된 비밀번호를 반환
+
+export const checkPassword = (salt, originPassword) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      crypto.pbkdf2(originPassword, salt, 10000, 64, "sha512", (err, key) => {
+        if (err) throw err;
+        resolve(key.toString("base64"));
+      });
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  });
+
+//유저의 salt 와 패스워드를 이용해 패스워드 체크에 사용!
+
+//db에 salt 와 해쉬화된 패스워드를 저장!!!
--- src/service/authService.js
+++ src/service/authService.js
@@ -1,14 +1,18 @@
 import client from "../client/client";
+import { createHashPassword } from "../middleware/password";
 
 export default {
   signUp: async (user) => {
     const { password, email, gender, birth } = user;
+    const { hashedPassword, salt } = await createHashPassword(password);
+    console.log(hashedPassword, salt);
     await client.user.create({
       data: {
-        password,
+        password: hashedPassword,
         email,
         gender,
         birth,
+        salt,
       },
     });
   },
